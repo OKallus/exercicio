2acc122657e7014b27a4a8daf761fd32e5996429
Karllus <**>
2025-09-09 02:08:39 -0300

    tarefa Wrappers

---
+++ tarefa_wrappers/src/tarefa_wrappers.java
@@ -0,0 +1,15 @@
+
+
+public class tarefa_wrappers {
+
+    public static void main(String[] args){
+
+        int valorPrimitivo = 25;
+        Integer valorWrapper = valorPrimitivo;
+
+
+        System.out.println("Valor primitivo: " + valorPrimitivo);
+        System.out.println("Valor wrapper (Integer): " + valorWrapper);
+    }
+
+}
